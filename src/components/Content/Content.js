a9c4c8367f88b1672c210659f23073ab4e50c5dd
react-developer <aarti.rewaatechverge@gmail.com>
2022-04-28 18:05:16 +0530

    some changes responsive

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -6,29 +6,38 @@ import rectangle from '../../images/Rectangle.svg'
 import cyclegif from '../../images/CyclingGIF.gif'
 import scooter from '../../images/Scooter 1.svg'
 // import Ellipse from '../../images/Ellipse9.png'
+import { Container, Row, Image, Col } from 'react-bootstrap'
 
 const Content = () => {
     return (
-        <div>
-            <div className='header'>
-                <img src={ImageArrow} className='imagearrow' alt='imagearrow' />
-                <div className='text col-7'>
-                    <h1 className='text-first text-nowrap'>Moving India <br />towords a <br /><div className='content-text-color'>sustainable future</div> </h1>
-                </div>
-                <div className='image col-5'>
-                    <img src={Mobile} className='mobileimage' alt="mobileImage" />
-                </div>
+        <Container fluid>
+            <div className="header">
+
+                <Row className='mt-5'>
+                    <Col >
+                        <Image src={ImageArrow} className="imagearrow" />
+                    </Col>
+                </Row>
+                <Row className='mt-5' xs={2} md='auto' lg='auto'>
+                    <Col >
+                        <h1 className='text-first text-nowrap'>Moving India <br />towords a <br /><div className='content-text-color'>sustainable future</div> </h1>
+                    </Col>
+                    <Col>
+                        <Image src={Mobile} className='mobileimage' alt="mobileImage" />
+                    </Col>
+                    <Col className='cycle'>
+                        <Image src={rectangle} className='rectangle-image' />
+                        <Image src={cyclegif} className='cycle-image' />
+                    </Col>
+
+                    <Col className=' scooter'>
+                        <Image src={scooter} />
+                    </Col>
+
+                </Row>
             </div>
-            <div className='row'>
-                <div className='cycle col-6'>
-                    <img src={rectangle} className='rectangle-image' />
-                    <img src={cyclegif} className='cycle-image' />
-                </div>
-                <div className='scooter col-6'>
-                    <img src={scooter} className='scooter' />
-                </div>
-            </div>
-        </div>
+        </Container>
+
     )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,20 +7,22 @@ import Content from '../Content/Content'
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="" className='nav-bar' variant="light">
-                <Container className='d-flex justify-content-around'>
-                    <Navbar.Brand href="#home"><img src={logo} className='nav-logo' alt='logo' /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav me-auto text-left" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ml-auto  text-right ">
+            <Container className='d-flex justify-content-around'>
+                <Navbar collapseOnSelect expand="lg" bg="" fixed='top' className='nav-bar' variant="light">
+
+                    <Navbar.Brand href="#home" className='ml-auto'><img src={logo} className='nav-logo' alt='logo' /></Navbar.Brand>
+                    <Navbar.Toggle className='ms-auto' aria-controls="responsive-navbar-nav text-left"  />
+                    <Navbar.Collapse  id="responsive-navbar-nav">
+                        <Nav className="ms-auto  text-right ">
                             <Nav.Link href='#/'>Home</Nav.Link>
                             <Nav.Link href="/#About">About Us</Nav.Link>
                             <Nav.Link href="/#Contact">Contact Us</Nav.Link>
                             <Nav.Link href="#Support">Support</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
+
+                </Navbar>
+            </Container>
             <Content />
         </div>
     )
